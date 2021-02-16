49cdde1e174e553c41a30a8ff5cb1c9fcc700657
rl <rl2015ab@gmail.com>
2021-02-16 16:48:32 -0700

    working version; but still needs commentary

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,20 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         ## first test comment
+    ##  the <<- operator which can be used to assign a value to an object in an environment that is different from the current environment. 
+    
+    matrixInverse <- NULL
+    set <- function(y) {
+        x <<- y
+        matrixInverse <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) matrixInverse <<- solve
+    getInverse <- function() matrixInverse
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+    
 }
 
 
@@ -12,4 +26,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    matrixInverse <- x$getInverse()
+    if(!is.null(matrixInverse)) {
+        message("getting cached data")
+        return(matrixInverse)
+    }
+    data <- x$get()
+    matrixInverse <- solve(data, ...)
+    x$setInverse(matrixInverse)
+    matrixInverse
 }
